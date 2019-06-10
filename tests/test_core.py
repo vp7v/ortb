f36ffc4a6c5619137f5e8b6510f19e175494f50c
Vladimir Plotnikov <23232563+vp7v@users.noreply.github.com>
2019-06-10 23:16:39 +0300

    Test example

---
+++ tests/test_core.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+import json
+
+from core import OrtbEncoder, OrtbArray, OrtbObject
+
+class TestCore(TestCase):
+
+    def test_OrtbEncoder(self):
+        class OrtbDuck(OrtbObject):
+            def __init__(self):
+                pass
+
+            def repr_json(self):
+                return 'Quack'
+
+        duck = OrtbDuck()
+        jsoned = json.dumps(duck, cls=OrtbEncoder)
+        self.assertEqual('"Quack"', jsoned)
+
+    def test_OrtbArray(self):
+        field = OrtbArray(str)
+        check = field([1, 2, '3'])
+        self.assertEqual(check, ['1', '2', '3'])
+
+        field = OrtbArray(int)
+        check = field(['1', 2, '3'])
+        self.assertEqual(check, [1, 2, 3])
